1453bd4120b5370ce1446689d2fe9ffc1e8e2c27
nikhiltnair <nikhiltnair@outlook.com>
2019-08-27 19:27:22 +0100

    Updates

---
+++ precache-manifest.919e383eb77ccc9e00ea6e112d8bd9ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "714673f975938a95e421",
+    "url": "/timeTableSchduling/static/js/runtime~main.b3f32e71.js"
+  },
+  {
+    "revision": "8db7e753bac8f343dc4a",
+    "url": "/timeTableSchduling/static/js/main.45dcb8fc.chunk.js"
+  },
+  {
+    "revision": "c58b58051026ba1d323b",
+    "url": "/timeTableSchduling/static/js/2.3419d985.chunk.js"
+  },
+  {
+    "revision": "8db7e753bac8f343dc4a",
+    "url": "/timeTableSchduling/static/css/main.08973b26.chunk.css"
+  },
+  {
+    "revision": "c58b58051026ba1d323b",
+    "url": "/timeTableSchduling/static/css/2.32da70d2.chunk.css"
+  },
+  {
+    "revision": "58dbb3da8976b213be02929aa03a7928",
+    "url": "/timeTableSchduling/index.html"
+  }
+];
